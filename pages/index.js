835edbe8779f1772a5c6e901b9ed8eb3d6e242d9
rfranca86 <universius@gmail.com>
2024-08-07 19:43:42 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Otto, eu amo você e fico muito feliz e realizado quando você se comporta e volta feliz da escola. Te amo meu filho querido. Se gostou da uma risadinha ! :)</h1>
+}
+
+export default Home;
