5abee7fdd92714e548310427504727eb8d42db21
rfranca86 <universius@gmail.com>
2024-08-29 03:51:43 +0000

    adiciona scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Otto se comporte bem todo dia e siga assim ! Te amo :)</h1>
+  return <h1>Otto se comporte bem todo dia e siga assim ! Te amo :)</h1>;
 }
 
 export default Home;
